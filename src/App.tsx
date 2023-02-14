375c69741778da0684fb6efbbc152ac73f510cb9
Your Name <you@example.com>
2023-02-14 09:45:58 +0100

    Betöltés, űrlap, listázás

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  etelek: Etel[];
+  etelNev: string;
+  etelKaloria: number;
 }
+
+interface Etel {
+  id: number;
+  nev: string;
+  kaloria: number;
+}
+
+interface etelListResponse {
+  etelek: Etel[];
+}
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      etelNev: '',
+      etelKaloria: 0,
+      etelek: []
+    }
+  }
+
+  async loadEtelek() {
+    let response = await fetch('http://localhost:3000/etel');
+    let data = await response.json() as Etel[];
+    this.setState({
+      etelek: data,
+    })
+  }
+
+  componentDidMount() {
+    this.loadEtelek();
+  }
+
+  handleRegister = async () => {
+    const { etelNev, etelKaloria } = this.state;
+    if (etelNev.trim() == '' || etelKaloria < 0) {
+      return;
+    }
+
+    const adat = {
+      nev: etelNev,
+      kaloria: etelKaloria
+    };
+
+    let response = await fetch('http://localhost:3000/etel', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(adat),
+    });
+
+    this.setState({
+      etelNev: '',
+      etelKaloria: 0,
+    })
+
+    await this.loadEtelek();
+  }
+
+  render() {
+    const { etelNev, etelKaloria } = this.state;
+
+    return <div>
+      <h2>Új étel</h2><br />
+      Név: <input type="text" value={ etelNev } onChange={e => this.setState({ etelNev: e.currentTarget.value })} /><br />
+      Kalória: <input type="number" value={ etelKaloria } onChange={e => this.setState({ etelKaloria: parseInt(e.currentTarget.value) })} /><br />
+      <button onClick={ this.handleRegister }>Felvétel</button><br />
+      <h2>Ételek listája</h2><br />
+      <ul>
+        {
+          this.state.etelek.map(etel => <li>{etel.nev}</li>)
+        }
+      </ul>
+    </div>
+  }
+}
+
 
 export default App;
